802794311713b3b5814ad498df5fc7600d05813f
visionamorg <abdo.elmahdi9901@gmail.com>
2026-03-27 13:38:53 +0100

    feat: Initialize the UrbanRunners SwiftUI application with basic project structure and default assets.

---
+++ UrbanRunners/UrbanRunners/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UrbanRunners
+//
+//  Created by macbook on 26/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UrbanRunners/UrbanRunners/UrbanRunnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanRunnersApp.swift
+//  UrbanRunners
+//
+//  Created by macbook on 26/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct UrbanRunnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
